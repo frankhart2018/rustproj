ccdc2daac0068885cc37de36d131c2678b76d962
frankhart <siddharthadhar.soumen@srmuniv.edu.in>
2021-04-16 20:07:47 +0530

    Add option to check version

--- rustproj/run.py
+++ rustproj/run.py
@@ -1,6 +1,8 @@
 import argparse
 import os
+import sys
 
+from . import __version__
 from .proj_generator import ProjGenerator
 from .utils import error
 
@@ -11,11 +13,17 @@ def run():
     parser.add_argument("--lib", action="store_true", default=False)
     parser.add_argument("--both", action="store_true", default=False)
     parser.add_argument("--force", action="store_true", default=False)
+    parser.add_argument("--version", action="store_true", default=False)
 
     args = parser.parse_args()
 
-    if args.proj == "":
+    if args.proj == "" and not args.version:
         error.throw_error(msg=f"Project name is required!")
+    elif args.proj != "" and args.version:
+        error.throw_error(msg=f"Cannot check version while creating project!")
+    elif args.proj == "" and args.version:
+        print(f"Rustproj version: \033[1m{__version__}\033[m")
+        sys.exit()
 
     if os.path.exists(args.proj) and not args.force:
         error.throw_error(msg=f"The project {args.proj} already exists, use --force to overwrite it!")
